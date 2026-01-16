b8c689b6e45940462df3f94c5f2f2e4108db8a85
kohta <kohtawakaida@gmail.com>
2026-01-16 11:14:16 +0900

    プログラミング入門2

---
+++ 1-2.test-java-entry2/src/Exam1.java
@@ -0,0 +1,13 @@
+public class Exam1 {
+    public static void main(String[] args) {
+        int totalScore = 48;
+
+        if (totalScore >= 100) {
+            System.out.println("満点です");
+        } else if (totalScore >= 80) {
+            System.out.println("合格です");
+        } else {
+            System.out.println("追試です");
+        }
+    }
+}
