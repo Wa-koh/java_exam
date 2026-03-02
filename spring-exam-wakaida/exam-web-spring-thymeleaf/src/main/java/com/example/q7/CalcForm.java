f1f9a0f19c288897b972c1c33cb8b07c957559d5
kohta <kohtawakaida@gmail.com>
2026-03-02 11:41:19 +0900

    ロジック & Spring テスト

---
+++ spring-exam-wakaida/exam-web-spring-thymeleaf/src/main/java/com/example/q7/CalcForm.java
@@ -0,0 +1,29 @@
+package com.example.q7;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+
+public class CalcForm {
+	@NotBlank(message = "入力は必須です。")
+	@Digits(integer = Integer.MAX_VALUE, fraction = 0, message = "左のテキストボックスには数値を入力してください。")
+	private String left;
+	@NotBlank(message = "入力は必須です。")
+	@Digits(integer = Integer.MAX_VALUE, fraction = 0, message = "右のテキストボックスには数値を入力してください。")
+	private String right;
+
+	public String getLeft() {
+		return left;
+	}
+
+	public void setLeft(String left) {
+		this.left = left;
+	}
+
+	public String getRight() {
+		return right;
+	}
+
+	public void setRight(String right) {
+		this.right = right;
+	}
+}
